c9339483d38f837cf21ab8b4153ee9e3befd52c7
Sneha Shukla <snehashukladec2002@gmail.com>
2026-08-16 01:13:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (67.83%) - LeetSync

---
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        vector<int> freq(26, 0);
+
+        for (char ch : s) {
+            if (freq[ch - 'a'] == 1) {
+                return ch;
+            }
+
+            freq[ch - 'a']++;
+        }
+
+        return ' ';
+    }
+};
